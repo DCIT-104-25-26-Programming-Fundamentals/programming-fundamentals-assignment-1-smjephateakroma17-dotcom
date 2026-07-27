efdae9c31a9892b6be943f4e772c88309d60c3f2
smjephateakroma17-dotcom <smjephateakroma17@gmail.com>
2026-07-27 17:18:40 +0000

    completed assignment 03

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -39,3 +39,58 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+
+
+num_list = []
+
+def main():
+    number = int (input("how many numbers? "))
+    i = 1
+
+    if number <=0 :
+        print("enter a number greater than 0")
+        return 
+
+    while i <= number:
+        input_score = int(input(f"Enter number {i}: "))
+        num_list.append(input_score)
+        i += 1
+
+    print("\nResults:")
+    sum_function()
+    average()
+    maximum()
+    minimum()
+
+def sum_function():
+    total_sum = 0 
+    for num in num_list:
+        total_sum += num
+    print(f"sum: {total_sum}")
+
+
+def average():
+    total_average = 0
+    for num in num_list:
+        total_average += num
+
+    total_average /= len(num_list)
+    print(f"Average: {total_average}")    
+
+def maximum():
+    max_value = num_list[0]
+    for i in range(1, len(num_list)):
+        if max_value < num_list[i]:
+            max_value = num_list[i]
+    print(f"maximum: {max_value}")
+
+def minimum():
+    minimum_value = num_list[0]
+    for i in range(1, len(num_list)):
+        if minimum_value > num_list[i]:
+            minimum_value = num_list[i] 
+    print(f"maximum: {minimum_value}")
+
+
+
+main()
